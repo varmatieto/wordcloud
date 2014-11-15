293a39dd686bd4bcfac0a7db5e427053a6f1c2fe
varmatieto <bixionino22@gmail.com>
2014-11-15 17:38:34 +0100

    this is what is on web

---
+++ tolearn.R
@@ -0,0 +1,16 @@
+
+library(tm)         # tm: Text Mining Package
+
+library(wordcloud)   # wordcloud: Word Clouds
+
+
+library(memoise)   # memoise: Memoise functions
+
+# Cache the results of a function so that when you call it again
+# with the same arguments it returns the pre-computed value.
+
+
+
+
+book <- c ("summer")
+sprintf("./%s.txt.gz", book)
